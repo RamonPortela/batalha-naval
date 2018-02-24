e09e1872cf0e08834aa2a9e1b380f2ace487bb5d
Renan Oliveira <renan.oliveira_10@hotmail.com>
2018-02-23 22:52:32 -0300

    adicionando propriedades e ações na classe jogador

--- jogador.py
+++ jogador.py
@@ -9,7 +9,7 @@ class Jogador:
 
 
     def verificarSeTiroAcertou(self, linha, coluna):
-        if(self.campo[linha - 1][coluna - 1] == 1):
+        if(self.campo[linha][coluna] == 1):
             return True
         else:
             return False
@@ -19,9 +19,21 @@ class Jogador:
             if(coluna + tamanho > 10):
                 return False
             while tamanho > 0:
-                self.campo[linha][coluna++] = 1
-                tamanho--
+                self.campo[linha][coluna] = 1
+                tamanho = tamanho - 1
+                coluna = coluna + 1
         else:
-            
-            
-        
+            if(linha + tamanho > 10):
+                return False
+            while tamanho > 0:
+                self.campo[linha][coluna] = 1
+                linha = linha + 1
+                tamanho = tamanho - 1
+        return True
+
+    def atirar(self):
+        linha = -1
+        while True:
+            linha = input('Insira a linha que deseja atirar')
+            if linha < 0 and linha > 9:
+                break
